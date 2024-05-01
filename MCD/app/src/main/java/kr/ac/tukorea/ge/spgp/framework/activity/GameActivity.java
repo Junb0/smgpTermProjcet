4fca04ab84d543b401bed2536a1405d3e8fa0e31
Junb0 <junb0sday@gmail.com>
2024-05-01 21:02:20 +0900

    feat: 프레임워크 추가

---
+++ MCD/app/src/main/java/kr/ac/tukorea/ge/spgp/framework/activity/GameActivity.java
@@ -0,0 +1,33 @@
+package kr.ac.tukorea.ge.spgp.framework.activity;
+
+import android.os.Bundle;
+
+import androidx.activity.OnBackPressedCallback;
+import androidx.appcompat.app.AppCompatActivity;
+
+import kr.ac.tukorea.ge.spgp2024.framework.view.GameView;
+
+public class GameActivity extends AppCompatActivity {
+
+    public static GameActivity activity;
+    private GameView gameView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        activity = this;
+        gameView = new GameView(this);
+        //gameView.setFullScreen();
+        setContentView(gameView);
+        //new MainScene().push();
+
+        getOnBackPressedDispatcher().addCallback(onBackPressedCallback);
+    }
+
+    private final OnBackPressedCallback onBackPressedCallback = new OnBackPressedCallback(true) {
+        @Override
+        public void handleOnBackPressed() {
+            gameView.onBackPressed();
+        }
+    };
+}

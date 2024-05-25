5f8c5319cbeb1cf205df6c5368226ead641179ba
Junb0 <junb0sday@gmail.com>
2024-05-25 17:28:38 +0900

    Feat: Bullet 상속구조 추가

--- MCD/app/src/main/java/kr/ac/tukorea/ge/spgp/memecatdefense/game/scene/main/Bullet.java
+++ MCD/app/src/main/java/kr/ac/tukorea/ge/spgp/memecatdefense/game/scene/main/Bullet.java
@@ -1,4 +1,30 @@
 package kr.ac.tukorea.ge.spgp.memecatdefense.game.scene.main;
 
-public class Bullet {
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+
+import kr.ac.tukorea.ge.spgp.framework.interfaces.IRecyclable;
+import kr.ac.tukorea.ge.spgp.framework.objects.Sprite;
+import kr.ac.tukorea.ge.spgp.framework.view.Metrics;
+import kr.ac.tukorea.ge.spgp.memecatdefense.R;
+
+public class Bullet extends Sprite implements IRecyclable {
+    public Bullet(){
+        super(R.mipmap.enemy_square);
+        srcRect = new Rect();
+    }
+    @Override
+    public void update(float elapsedSeconds) {
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        super.draw(canvas);
+    }
+    @Override
+    public void onRecycle() {
+
+    }
 }
